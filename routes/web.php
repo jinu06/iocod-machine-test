512e94b89579d6c73a08dde3d217db6de7ff6088
Jinshaj <jinuwdr05@gmail.com>
2025-08-05 23:34:33 +0530

    Add BankStatement model, controller, and migration for bank statements

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\BankStatementController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::controller(BankStatementController::class)->group(function () {
+    Route::get("/", "index")->name("Welcome");
 });
 
 Route::prefix('api')->group(function () {
