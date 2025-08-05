4f2731330df29930c831ab295d9e260796cd31f7
Jinshaj <jinuwdr05@gmail.com>
2025-08-05 16:03:09 +0530

    Refactor code structure for improved readability and maintainability

---
+++ app/Actions/ImportLeads.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\Lead;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+
+class ImportLeads
+{
+    public function handle(): void
+    {
+        try {
+            $path = storage_path('leads.json');
+
+            if (!file_exists($path)) {
+                throw new \Exception('The Mock file is not found');
+            }
+
+            $json = file_get_contents($path);
+            $records = json_decode($json, true);
+            DB::beginTransaction();
+
+            $batch = [];
+            foreach ($records as $record) {
+                $batch[] = [
+                    'merchant_name' => $record['merchant_name'],
+                    'requested_amount' => $record['requested_amount'],
+                    'lead_score' => $record['lead_score'],
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+
+                if (count($batch) >= 1000) { // hopefully works for millions of record
+                    Lead::insert($batch);
+                    $batch = [];
+                }
+            }
+
+            if (!empty($batch)) {
+                Lead::insert($batch);
+            }
+
+            DB::commit();
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            Log::error($th->getMessage());
+            throw $th;
+        }
+    }
+}
